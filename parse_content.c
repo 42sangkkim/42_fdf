ea9ee0bdad241876b2c75ddddd0d73b50f4ee035
sangkkim <48117646+42sangkkim@users.noreply.github.com>
2022-06-22 10:01:08 +0900

    Update parse_content.c
    성공했을 때의 알고리즘만 완성
    arrlen, free_arr 등의 tool 함수 필요
    실패했을 때 exit일지 return일지 안정함 (어차피 다 malloc error)

--- parse_content.c
+++ parse_content.c
@@ -17,10 +17,46 @@ char	***parse_content(char *content)
 	char	**lines;
 	char	***words;
 
+	lines = parse_nl(content);
+	words = ft_calloc(ft_arrlen(lines) + 1, sizeof(char **));
+	if (!words)
+		// err
+	i = 0;
+	while (lines[i])
+	{
+		words[i] = ft_split(lines[i], ' ');
+		if (words[i])
+		{
+			while(i--)
+				free_arr(words[i]);
+			free(words);
+			free(*lines);
+			free(lines);
+		}
+		i++;
+	}
+	free(*lines);
+	free(lines);
+	return (words);
+}
+
+
+char	**parse_nl(char *content)
+{
+	char	**lines;
+
 	lines = ft_calloc(count_nl(content) + 2, sizeof(char *));
 	if (!lines)
 		// malloc error
 	lines[0] = content;
-	i = 0;
-	while (lines[i]
-	lines[1]
+	i = 1;
+	while (1)
+	{
+		lines[i] = ft_strchr(lines[i - 1], '\n');
+		if (lines[i] == NULL)
+			break ;
+		*(lines[i]++) = '\0';
+		i++;
+	}
+	return (lines);
+}
